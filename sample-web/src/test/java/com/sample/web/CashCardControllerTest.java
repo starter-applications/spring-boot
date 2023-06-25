bbc64e10874003bfcd6f461c2b141e006219fc93
Vikrant Verma <vikrant.verma2002@gmail.com>
2023-06-25 13:34:36 +0400

    owner column added everywhere

--- sample-web/src/test/java/com/sample/web/CashCardControllerTest.java
+++ sample-web/src/test/java/com/sample/web/CashCardControllerTest.java
@@ -47,7 +47,7 @@ public class CashCardControllerTest {
     @Test
     @DirtiesContext
     void shouldCreateANewCashCard() {
-        CashCard newCashCard = new CashCard(null, 250.00);
+        CashCard newCashCard = new CashCard(null, 250.00, "sarah1");
         ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
         assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
--- sample-web/src/test/java/com/sample/web/CashCardTest.java
+++ sample-web/src/test/java/com/sample/web/CashCardTest.java
@@ -7,8 +7,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CashCardTest {
     @Test
     public void myFirstTest() {
-        CashCard cashCard = new CashCard(99L, 123.45);
+        CashCard cashCard =  new CashCard(99L, 123.45, "sarah1");
         assertThat(cashCard.id()).isEqualTo(99L);
         assertThat(cashCard.amount()).isEqualTo(123.45);
+        assertThat(cashCard.owner()).isEqualTo("sarah1");
     }
 }
